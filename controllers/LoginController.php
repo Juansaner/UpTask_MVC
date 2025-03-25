fd29153b84f6dd62ffa796f432d343c77f45b6c4
Juan Pablo <juansaner01@gmail.com>
2025-03-25 18:07:35 -0500

    Create login view

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,26 +1,32 @@
 <?php
 
 namespace Controllers;
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo "Desde Login";
-
+    public static function login(Router $router) {
         if($_SERVER['REQUEST_METHOD']) {
 
         }
+
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar sesión'
+        ]);
     }
 
     public static function logout() {
         echo "Desde Logout";
     }
 
-    public static function crear() {
-        echo "Desde crear";
+    public static function crear(Router $router) {
 
         if($_SERVER['REQUEST_METHOD']) {
 
         }
+
+        $router->render('auth/crear', [
+            'titulo' => 'Crear cuenta'
+        ]);
     }
 
     public static function olvide() {
