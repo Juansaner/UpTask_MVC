edda690fc213241853c9c57f4f1898c294f508b6
Juan Pablo <juansaner01@gmail.com>
2025-03-28 10:47:39 -0500

    feat(confirmar): create initial structure for the 'confirmar' page and adjust CSS

---
+++ views/auth/confirmar.php
@@ -0,0 +1,14 @@
+<div class="contenedor-login">
+        <!-- Imagen a la izquierda -->
+        <div class="imagen"></div>
+
+        <!-- Formulario a la derecha -->
+        <div class="formulario-contenedor confirmar">
+            <?php include_once __DIR__ . "/../templates/nombre-sitio.php"; ?>
+            <div class="contenedor-sm">
+                <div class="acciones">
+                    <a href="/">Iniciar sesión</a>
+                </div>
+            </div><!-- .contenedor-sm -->
+        </div><!-- .formulario-contenedor -->
+</div><!-- .contenedor-login -->
