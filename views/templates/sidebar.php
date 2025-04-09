5c895acc60cf9d7807f17ce0b33016147445cfbe
Juan Pablo <juansaner01@gmail.com>
2025-04-09 12:40:35 -0500

    Creting the dashboard view

---
+++ views/templates/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+    <h2>TaskFlow</h2>
+    <nav class="sidebar-nav">
+        <a href="/dashboard">Proyectos</a>
+        <a href="/crear-proyecto">Crear proyecto</a>
+        <a href="/perfil">Perfil</a>
+    </nav>
+</aside>
