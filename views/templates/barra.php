4ebf98c2ece9fbf56ef036d4d620aac31a251248
Juan Pablo <juansaner01@gmail.com>
2025-04-10 08:06:06 -0500

    style(dashboard): applying CSS to the button 'logout'

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,4 +1,4 @@
 <div class="barra">
     <p>Hola <span><?php echo $_SESSION['nombre'];?></span></p>
-    <a href="/logout" class="cerrar-sesion">Cerrar sesión</a>
+    <a href="/logout" class="cerrar-sesion"><i class="fa-solid fa-right-from-bracket"></i> Cerrar sesión</a>
 </div>
