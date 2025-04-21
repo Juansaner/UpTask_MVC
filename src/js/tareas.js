6895eb54d4a7a48445cbe55e6a1c571247d4fe0b
Juan Pablo <juansaner01@gmail.com>
2025-04-21 18:29:55 -0500

    Create a form to add tasks

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -3,6 +3,21 @@ const nuevaTareaBtn = document.querySelector('#nueva-tarea');
 nuevaTareaBtn.addEventListener('click', mostrarFormulario);
 
 function mostrarFormulario() {
-    console.log('Mostrar formulario');
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.innerHTML = `
+        <form class='formulario'>
+            <legend>Añadir nueva tarea</legend>
+            <div class='campo'>
+                <label>Tarea</label>
+                <input type='text' id='tarea' name='tarea' placeholder='Añadir tarea al proyecto actual'  />
+            </div>
+            <div class='opciones'>
+                <input type='submit' class='submit-nueva-tarea' value='Añadir tarea' />
+                <button type='button' class='cerrar-modal'>Cancelar</button>
+            </div>
+        </form>
+        `;    
+        document.querySelector('body').appendChild(modal);
 }
 })();
