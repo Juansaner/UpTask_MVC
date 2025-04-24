5f1faf4156e3077dabef599bc6554b4d68fd0c96
Juan Pablo <juansaner01@gmail.com>
2025-04-23 21:15:49 -0500

    refactor(Tarea): rename the column

--- models/Tarea.php
+++ models/Tarea.php
@@ -4,12 +4,12 @@ namespace Model;
 
 class Tarea extends ActiveRecord {
     protected static $tabla = 'tareas';
-    protected static $columnasDB = ['id', 'titulo', 'estado', 'proyectoId'];
+    protected static $columnasDB = ['id', 'nombre', 'estado', 'proyectoId'];
 
     public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
-        $this->titulo = $args['titulo'] ?? '';
+        $this->nombre = $args['nombre'] ?? '';
         $this->estado = $args['estado'] ?? 0;
         $this->proyectoId = $args['proyectoId'] ?? '';
     }
